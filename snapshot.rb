49674afe7fa8654caa2af5491679fa83f269aad2
MarcelRusu <marcelrusu@protonmail.com>
2021-12-27 18:31:17 -0500

    add snapshot specs

---
+++ snapshot.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.push "./lib"
+
+require "lexer"
+require "parser"
+
+content = File.read(ARGV[0])
+tokens = Lexer::tokenize(content)
+ast = AST::remove_numbers(Parser.new(tokens).parse!)
+spec = <<-EOS
+  it "#{Time::now}" do
+    ast = parse("#{content}")
+    expect(ast).to ast_eq(#{ast})
+  end
+EOS
+
+SNAPSHOT_SPEC_FILENAME = "spec/snapshot_spec.rb"
+
+*file, end_line = File.read(SNAPSHOT_SPEC_FILENAME)
+  .split("\n")
+
+throw "wtf" if end_line != "end"
+
+new_file = (file + spec.split("\n") + [end_line]).join("\n")
+
+File.write(SNAPSHOT_SPEC_FILENAME, new_file)
