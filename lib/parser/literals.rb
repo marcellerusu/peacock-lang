2437eed2b25b3c84cad8c66184514f392a246e11
MarcelRusu <marcelrusu@protonmail.com>
2022-03-12 13:28:56 -0500

    clean up str capture parsing

--- lib/parser/literals.rb
+++ lib/parser/literals.rb
@@ -5,7 +5,7 @@ module Literals
       .to_schema_any_depending_on(expr_context)
       .to_instance_method_lookup_depending_on(parser_context)
     return expr if expr_context.in_a? :schema
-    parse_id_modifier_if_exists!(expr)
+    parse_id_modifier_if_exists! expr
   end
 
   def parse_property!
@@ -43,33 +43,50 @@ module Literals
 
   def parse_str!
     str_token = consume! :str_lit
-    str_expr = if str_token.captures.size == 0
-        AST::Str.new str_token.value, str_token.position
-      else
-        strings = []
-        strings.push AST::Str.new(
-          str_token.value[0...str_token.captures.first[:start] - 2],
-          str_token.position
-        )
-        str_token.captures.each_with_index do |group, i|
-          ast = clone(
-            tokens: group[:tokens],
-            token_index: 0,
-            parser_context: parser_context.push(:str),
-          ).parse!
-          assert { ast.size == 1 }
-          strings.push ast.first.call_to_s
-          if i + 1 < str_token.captures.size
-            strings.push AST::Str.new(str_token.value[group[:end] + 1...str_token.captures[i + 1][:start] - 2], nil)
-          end
-        end
-        strings.push AST::Str.new(str_token.value[str_token.captures.last[:end] + 1..], nil)
-
-        strings.reduce do |str, cur|
-          str.plus(cur)
-        end
-      end
+    str_expr = parse_str_interpolation! str_token
     parse_id_modifier_if_exists! str_expr
+  end
+
+  def parse_str_interpolation!(str_token)
+    if str_token.captures.size == 0
+      return AST::Str.new str_token.value, str_token.position
+    end
+    strings = []
+    strings.push str_before_captures(str_token)
+    str_token.captures.each_with_index do |group, i|
+      ast = clone(
+        tokens: group[:tokens],
+        token_index: 0,
+        parser_context: parser_context.push(:str),
+      ).parse!
+      assert { ast.size == 1 }
+      strings.push ast.first.call_to_s
+      strings.push str_between_escape(str_token, i)
+    end
+    strings.push AST::Str.new(str_token.value[str_token.captures.last[:end] + 1..], nil)
+
+    strings.compact.reduce do |str, cur|
+      str.plus(cur)
+    end
+  end
+
+  def str_before_captures(str_token)
+    # in example "oh shit #{1} something #{2}"
+    # we're parsing "oh shit " with this condition
+    start_index = 0
+    # - 2 is to ignore `#{`
+    end_index = str_token.captures[0][:start] - 2
+    AST::Str.new(str_token.value[start_index...end_index], str_token.position)
+  end
+
+  def str_between_escape(str_token, i)
+    return nil if i + 1 == str_token.captures.size
+    # in example "oh shit #{1} something #{2}"
+    # we're parsing " something " with this condition
+    start_index = str_token.captures[i][:end] + 1
+    # - 2 is to ignore `#{`
+    end_index = str_token.captures[i + 1][:start] - 2
+    AST::Str.new(str_token.value[start_index...end_index], str_token.position)
   end
 
   def parse_record_key!
